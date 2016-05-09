f4d4ceb9a768d58bae6f1d6720a3a3d5ded4c1e4
Newton Jain <newton.jain@gmail.com>
2016-05-09 16:20:59 -0700

    First commit for the project

---
+++ app/app.ts
@@ -0,0 +1,23 @@
+/// <reference path="../typings/browser.d.ts" />
+
+// Ionic Starter App
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform: ionic.platform.IonicPlatformService) {
+  $ionicPlatform.ready(() => {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      window.StatusBar.styleLightContent();
+    }
+  });
+})
