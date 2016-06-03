80307298d033ddf4bd901f55b2bacc2ac4f77f39
ridomin <rmpablos@hotmail.com>
2016-06-03 10:22:58 -0700

    cleaning gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
 /// <binding BeforeBuild='default' />
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
 
 var paths = {
     sass: ['./scss/**/*.scss'],
@@ -54,11 +50,4 @@ gulp.task('tsc', function() {
         }          
       }))
 	  .pipe(gulp.dest('www/js/'));
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
 });
